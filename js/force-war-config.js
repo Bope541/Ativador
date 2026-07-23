120a66399e3f5db8bc7d5ce2f1e8b956af891831
Force War <forcewar646@gmail.com>
2026-07-23 15:43:55 -0300

    Update executor URL to version 4.6.2

--- js/force-war-config.js
+++ js/force-war-config.js
@@ -1,4 +1,4 @@
 window.FW_SITE_CONFIG = {
   version: 'v4.6.2',
-  executorUrl: 'https://github.com/Bope541/Executor-Force-War/releases/download/v4.6.1/ForceWar.exe'
+  executorUrl: 'https://github.com/Bope541/Executor-Force-War/releases/download/v4.6.2/ForceWar.exe'
 };
